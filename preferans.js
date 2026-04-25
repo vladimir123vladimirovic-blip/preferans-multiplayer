4de273030717bb2dcb2f0c4e2eba91b38ef676e6
vladimir123vladimirovic-blip <vladimir123vladimirovic@gmail.com>
2026-04-26 01:18:37 +0200

    Update preferans.js

--- preferans.js
+++ preferans.js
@@ -5,7 +5,7 @@ const { Server } = require('socket.io');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 const server = http.createServer(app);
 const io = new Server(server);
 
